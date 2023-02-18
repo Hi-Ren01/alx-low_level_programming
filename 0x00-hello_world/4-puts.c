b22b20162599a6e3a479bd7ae3bb2cb07d6f43bc
Hi_Ren <onyerene1999@yahoo.com>
2023-02-18 11:15:30 -0800

    Hello,Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This returns a new line wih puts function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+returun(0);
+}
+
